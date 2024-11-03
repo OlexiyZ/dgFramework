a85f09b7d97de0442b1791d47f5e52fa440280a5
Olexiy Zyuzyukov <a.zyuzyukov@gmail.com>
2024-11-03 13:11:58 +0200

    Database configuration correction in settings.py file

--- dgFramework/settings.py
+++ dgFramework/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-o!46yfbflocr&c9s3z8(azkfzrilj*z+c79g^5@!7xhu!(5s($"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if platform.system() == "Linux":
-    DEBUG = False
-else:
-    DEBUG = True
+# if platform.system() == "Linux":
+#     DEBUG = False
+# else:
+#     DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -82,7 +84,26 @@ WSGI_APPLICATION = "dgFramework.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# if DEBUG:
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "dg_bae",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "localhost",
+            "PORT": "5432"
+        }
+    }
+
+
+# if platform.system() == "Linux":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
 #     DATABASES = {
 #         "default": {
 #             "ENGINE": "django.db.backends.postgresql_psycopg2",
@@ -93,36 +114,6 @@ WSGI_APPLICATION = "dgFramework.wsgi.application"
 #             "PORT": "5432"
 #         }
 #     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql_psycopg2",
-#             "NAME": "database-1",
-#             "USER": "postgres",
-#             "PASSWORD": "Postgres1",
-#             "HOST": "localhost",
-#             "PORT": "5432"
-#         }
-#     }
-
-if platform.system() == "Linux":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "dg_bae",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "localhost",
-            "PORT": "5432"
-        }
-    }
 
 
 # Password validation
