202b1a24595a1db1ee69b8601c07e1724f0a9a56
Olexiy Zyuzyukov <a.zyuzyukov@gmail.com>
2025-04-09 13:06:29 +0300

    requirements.txt update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-
-pip install "django>=5.0.7,<5.1"
-pip install mozilla-django-oidc
-pip install jwcrypto requests
 # Run migrations and create superuser
 python manage.py makemigrations
 python manage.py migrate
